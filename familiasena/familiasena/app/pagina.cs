044e18c71b2c0c3e28c087553e2b03aa8901a8dc
William Bernal <willtf.wb@gmail.com>
2016-05-03 05:46:19 -0500

    lol

--- familiasena/familiasena/app/pagina.cs
+++ familiasena/familiasena/app/pagina.cs
@@ -18,11 +18,16 @@ public partial class pagina : System.Web.UI.Page
                 infoFamiliaWS(Correo);
             }
         }
-        public void Logout()
-            {
+
+
+        public void Logout() {
                 Session.Abandon();
                 Response.Cookies.Add(new HttpCookie("ASP.NET_SessionId", ""));
             }
+    //control de cantidad de familiares
+
+
+
     protected void infoFamiliaWS(string e) {
 
         string Cedula = "";
@@ -36,34 +41,33 @@ public partial class pagina : System.Web.UI.Page
 
         ws.ConsFamilia[] resultado = Consulta.Consulta_Familia(Cedula, idfamiliar, Correo, CodAc);
 
-        //contenido.InnerHtml = "";
-
+        contenido.InnerHtml = "";
+        int i = 0;
         foreach (var item in resultado)
         {
             // llamado de los valores
-
-            // contenido.InnerHtml += "<h6>" + item.COD_EMPL + "</h6>";
-            //contenido.InnerHtml += "<h6>" + item.COD_FAMI + "</h6>";
-            //contenido.InnerHtml += "<h6>" + item.TIP_RELA + "</h6>";
-            // contenido.InnerHtml += "<h6>" + item.EEE_MAIL + "</h6>";
-            // contenido.InnerHtml += "<h6>" + item.APELLIDOS + "</h6>";
-            // contenido.InnerHtml += "<h6>" + item.NOMBRES + "</h6>";
-            // contenido.InnerHtml += "<h6>" + item.APE_FAMI + "</h6>";
-            // contenido.InnerHtml += "<h6>" + item.NOM_FAMI + "</h6>";
-            // contenido.InnerHtml += "<h6>" + item.RELACION + "</h6>";
-            // contenido.InnerHtml += "<h6>" + item.NACIO + "</h6>";
-            // contenido.InnerHtml += "<h6>" + item.EDAD + "</h6>";
-            //contenido.InnerHtml += "<h6>" + item.DEPENDE + "</h6>";
-            // contenido.InnerHtml += "<h6>" + item.DISCAP + "</h6>";
-            //contenido.InnerHtml += "<h6>" + item.REGIONAL + "</h6>";
-            //contenido.InnerHtml += "<h6>" + item.CENTRO + "</h6>";
-            //contenido.InnerHtml += "<h6>" + item.ACTUALIZO + "</h6>";
-            //contenido.InnerHtml += "<h6>" + item.Nro_Registros + "</h6>";
+            i++;
+            contenido.InnerHtml += "<h6>" + item.COD_EMPL + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.COD_FAMI + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.TIP_RELA + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.EEE_MAIL + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.APELLIDOS + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.NOMBRES + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.APE_FAMI + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.NOM_FAMI + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.RELACION + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.NACIO + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.EDAD + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.DEPENDE + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.DISCAP + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.REGIONAL + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.CENTRO + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.ACTUALIZO + "</h6>";
+            contenido.InnerHtml += "<h6>" + item.Nro_Registros + "</h6>";
+            contenido.InnerHtml += "<div class='onoffswitch' OnClick='check_Click' Value='"+item.COD_FAMI+"'><input type = 'checkbox' name = 'onoffswitch' class='onoffswitch-checkbox' id='myonoffswitch" + i+ "'><label class='onoffswitch-label' for='myonoffswitch" + i + "'><span class='onoffswitch-inner'></span><span class='onoffswitch-switch'></span></label></div>";
 
         }
 
 
     }
-    
-    
 }
